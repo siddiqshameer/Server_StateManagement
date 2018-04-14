2668b5d5d7cad3b41c7a684acc1e97947559a6e8
siddiq shameer <siddiqshameer@outlook.com>
2018-04-14 23:45:35 +0530

    added destination files
    close #2

--- Server_StateManagement/ApplicationStateSource.aspx.cs
+++ Server_StateManagement/ApplicationStateSource.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class ApplicationStateSource : System.Web.UI.Page
 {
@@ -14,6 +9,8 @@ public partial class ApplicationStateSource : System.Web.UI.Page
 
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
-
+        Application["Name"] = txtName.Text;
+        Application["Age"] = txtAge.Text;
+        Response.Redirect("AppplicationStateDest.aspx");
     }
 }
---
+++ Server_StateManagement/AppplicationStateDest.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public partial class AppplicationStateDest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            lblName.Text = "" + Application["Name"];
+            lblAge.Text = "" + Application["Age"];
+        }
+    }
+}
