d9ec7e8e858f1753e4c8e8df8a3619dd0bcdd408
siddiq shameer <siddiqshameer@outlook.com>
2018-04-14 23:18:22 +0530

    added session state destination files

---
+++ Server_StateManagement/SessionStateDest.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public partial class SessionStateDest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            if (Session["Name"] != null && Session["Course"] != null) 
+            {
+                lblName.Text = Session["Name"].ToString();
+                lblCourse.Text = Session["Course"].ToString();
+            }
+        }
+    }
+}
